95523c674734b1da5d0e1c0d008ad5d6b6500837
lakshmikant <lakshmikant2512@gmail.com>
2021-09-08 08:45:05 +0530

    add hamMenu transition

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,20 @@
 
-const hamBtn =  document.querySelector('.ham-div');
-const closeBtn = document.querySelector('.close-btn');
+const hamBtn =  document.querySelector('.ham-expand-div');
+const closeBtn = document.querySelector('.ham-contract-div');
 const  sideNav = document.querySelector('.side-navbar');
-const main = document.querySelector('main');
+const main = document.querySelector('.main');
 
 closeBtn.onclick = () =>{
-    sideNav.style.display="none";
-    hamBtn.style.display="unset";
-    main.style.marginLeft="90px";
+    sideNav.style.transform="translateY(-100%)";
+    hamBtn.style.transform="scale(1)";
+    main.style.transform="translateX(-200px)";
+    main.style.transition="transform .75s ease-in-out";
+    hamBtn.style.transition="transform .75s ease-in-out"
 }
 
 hamBtn.onclick = () =>{
-    sideNav.style.display="unset";
-    hamBtn.style.display="none"
-    main.style.marginLeft="300px";
+    sideNav.style.transform="translateY(0)";
+    hamBtn.style.transform="scale(0)";
+    main.style.transform="translateX(0)";
+    hamBtn.style.transition="transform .2s ease-in-out";
 }
