cbb856d69f7872a75d2aa351ce35d0e8d2b56c30
lakshmikant <lakshmikant2512@gmail.com>
2021-09-18 19:33:29 +0530

    add signOut function

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,11 @@
+import {firebaseConfig} from "./firebase-modules.js"
+firebase.initializeApp(firebaseConfig);
 
 const hamBtn =  document.querySelector('.ham-expand-div');
 const closeBtn = document.querySelector('.ham-contract-div');
 const  sideNav = document.querySelector('.side-navbar');
 const main = document.querySelector('.main');
+const signOutBtn = document.querySelector('#sign-out-btn');
 
 closeBtn.onclick = () =>{
     sideNav.style.transform="translateX(-100%)";
@@ -17,3 +20,11 @@ hamBtn.onclick = () =>{
     main.style.transform="translateX(0)";
     hamBtn.style.transition="transform .1s ease-in-out";
 }
+
+signOutBtn.addEventListener('click',()=>{
+    firebase.auth().signOut().then(function() {
+        window.location="./index.html"
+      }, function(error) {
+        console.error(error);
+      });
+})
