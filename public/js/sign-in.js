7424de2c678e71d9b2d079aaef18507b0e8f3f98
rahulkarthik <aurkkarthik27112001@gmail.com>
2021-09-18 14:16:58 +0530

    store user details

--- public/js/sign-in.js
+++ public/js/sign-in.js
@@ -10,20 +10,42 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-document.getElementById('google-sign-in').addEventListener('click', GoogleLogin)
-
+const googleSignIn = document.getElementById('google-sign-in')
+checkStatus()
+googleSignIn.addEventListener('click', googleLogin)
 let provider = new firebase.auth.GoogleAuthProvider()
 
-function GoogleLogin() {
+function checkStatus() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      writeInDb(user)
+    } else {
+    }
+  });
+}
+
+function googleLogin() {
   firebase.auth().signInWithPopup(provider).then(res => {
-    console.log(res.user) 
-    window.location.href="./index.html";
+    console.log(res.user)
   }).catch(e => {
     console.log(e)
   })
 }
 
+function writeInDb(user) {
+  const name = user.displayName
+  const imageUrl = user.photoURL
+
+  firebase.database().ref('users/' + user.uid).set({
+    username: name,
+    profile_picture: imageUrl
+  }, (error) => {
+    if (error) {
+    } else {
+      window.location = "./index.html"
+    }
+  });
+}
 window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
 
 recaptchaVerifier.render().then(widgetId => {
