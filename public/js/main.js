a904521060a867db14d55fdbccb7ba1efc3af862
lakshmikant <lakshmikant2512@gmail.com>
2021-09-28 17:52:45 +0530

    add user details form  --> sign-in page

--- public/js/main.js
+++ public/js/main.js
@@ -46,7 +46,7 @@ function signInWithPhone(sentCodeId) {
 
 auth.onAuthStateChanged((user) => {
   if (user) {
-    writeInDb(user)
+    userFormGenerate(user);
   }
 });
 
@@ -56,16 +56,39 @@ function googleLogin() {
   googleSignIn.removeEventListener('click', googleLogin);
   auth.signInWithPopup(provider).then(res => {
     console.log(res.user);
+    body.style.filter = "none";
   }).catch(e => {
     body.style.filter = "none";
     googleSignIn.addEventListener('click', googleLogin);
+  })
+}
 
+
+function userFormGenerate(user) {
+  const formDiv = document.querySelector('.form-cnt');
+  const formWrapper = document.querySelector('.form-wrapper')
+  formDiv.remove();
+  formWrapper.innerHTML = `
+    <input type='text' placeholder='Enter your name' id='u_name' />
+    <input type='text' placeholder='Door no' id='u_door_no' /> 
+    <input type='text' placeholder='Street name' id='u_street_name' />
+    <input type='text' placeholder='Area' id='u_area' /> 
+    <input type='text' placeholder='Pincode' id='u_pincode' /> 
+    <button id='user-det-btn'>Submit</button>`;
+  const userDetBtn = document.querySelector('#user-det-btn');
+  userDetBtn.addEventListener('click', ()=>{
+    writeInDb(user)
   })
 }
 
 function writeInDb(user) {
   const name = user.displayName
   const imageUrl = user.photoURL
+  const userName = document.querySelector('#u_name')
+  const userDoorNo = document.querySelector('#u-door-no')
+  const userStreetName = document.querySelector('#u-street-name')
+  const userArea = document.querySelector('#u-area')
+  const userPincode = document.querySelector('#u-pincode')
   firebase.database().ref('users/' + user.uid).set({
     username: name,
     profile_picture: imageUrl
