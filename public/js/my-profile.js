2a3e3d4836e1d3f4b76ddbd75baa90e2790d80b2
rahulkarthik <aurkkarthik27112001@gmail.com>
2021-09-29 13:32:14 +0530

    upload user image

--- public/js/my-profile.js
+++ public/js/my-profile.js
@@ -1,2 +1,16 @@
 import { firebaseConfig } from "./firebase-modules.js"
 firebase.initializeApp(firebaseConfig);
+const uploadImageBtn=document.querySelector('#upload-image')
+uploadImageBtn.addEventListener('click',uploadImage)
+function uploadImage(){
+    let uploadPhoto=document.querySelector("#user-photo-up").files[0]
+    firebase.storage().ref('Images').child(uploadPhoto.name).put(uploadPhoto).then(res => {
+        console.log("success")
+    }).catch(e=>{
+   console.log(e)
+    })
+
+}
+uploadImageBtn.addEventListener('click',uploadImage)
+
+
