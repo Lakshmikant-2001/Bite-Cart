354960d15f61ebbd315336bc4f17ad2dc62e2bc9
lakshmikant <lakshmikant2512@gmail.com>
2021-10-05 22:09:52 +0530

    refactor reference --> food Seller page

--- public/js/food-seller.js
+++ public/js/food-seller.js
@@ -1,6 +1,7 @@
 import { firebaseConfig } from "./firebase-modules.js"
 firebase.initializeApp(firebaseConfig);
 
+const database = firebase.database()
 const auth = firebase.auth()
 const storage = firebase.storage()
 
@@ -14,20 +15,20 @@ const foodType = document.querySelector('#food-type')
 const foodPrice = document.querySelector('#food-price') 
 const foodTotalQty = document.querySelector('#food-total-quantity') 
 const foodImage = document.querySelector('#food-img') 
-
 const addFoodBtn = document.querySelector('#add-food-btn') 
 const addResBtn = document.querySelector('#add-res-btn') 
 
 auth.onAuthStateChanged((user) => {
     if (user) {
-        addResBtn.addEventListener("click",()=>{
-            addRestaurantDet(user.uid)
-            uploadResImg(user.uid)
-
-        })
+        const dbref = `Food-Seller/${user.uid}`
+        const storageRef = `Restaurant/${user.uid}`
+        // addResBtn.addEventListener("change",()=>{
+        //     addRestaurantDet(dbref)
+        //     uploadResImg(storageRef)
+        // })
         addFoodBtn.addEventListener("click",()=>{
-            addFoodDet(user.uid)
-            uploadFoodImg(user.uid)
+            addFoodDet(dbref)
+            uploadFoodImg(storageRef)
         })
     }
     else {
@@ -35,17 +36,17 @@ auth.onAuthStateChanged((user) => {
     }
 });
 
-function addRestaurantDet(uid){
-    firebase.database().ref('Food-Seller/' + uid +'-'+ resName.value +'/Res-details/').set({
-        Res_name: resName.value,
-        Res_type: resType.value,
-        Res_location:resLocation.value,
-        Res_pincode:resPincode.value
-    })
+// function addRestaurantDet(dbref){
+//     database.ref(`${dbref}/Res_Det`).set({
+//         Res_name: resName.value,
+//         Res_type: resType.value,
+//         Res_location:resLocation.value,
+//         Res_pincode:resPincode.value
+//     })
+// }
 
-}
-function addFoodDet(uid){
-    firebase.database().ref('Food-Seller/' + uid +'-'+ resName.value +'/Foods/'+foodName.value).set({
+function addFoodDet(dbref){
+    database.ref(`${dbref}/Foods/${foodName.value}`).set({
         Food_name: foodName.value,
         Food_type: foodType.value,
         Food_price:foodPrice.value,
@@ -54,14 +55,16 @@ function addFoodDet(uid){
 
 }
 
-function uploadResImg(id) {
-    storage.ref("Restaurant/"+id).child("ResImage").put(resImage.files[0]).then(res => {
-    }).catch(e => {
-        console.log(e)
-    })
-}
-function uploadFoodImg(id) {
-    storage.ref("Restaurant/"+id+"/Foods/").child(foodName.value).put(foodImage.files[0]).then(res => {
+// function uploadResImg(storageRef) {
+    // storage.ref(`${storageRef}/Res-img`).put(resImage.files[0]).then(res => {
+//     }).catch(e => {
+//         console.log(e)
+//     })
+// }
+
+function uploadFoodImg(storageRef) {
+    storage.ref(`${storageRef}/Food-imgs`)
+    .child(foodName.value).put(foodImage.files[0]).then(res => {
     }).catch(e => {
         console.log(e)
     })
